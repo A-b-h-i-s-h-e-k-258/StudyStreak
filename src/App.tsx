5872f9701e37a0e2a94d924c75cf17cf76c933f6
Abhishek <158245223+A-b-h-i-s-h-e-k-258@users.noreply.github.com>
2025-06-25 12:28:28 +0000

    corrected Speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -27,8 +26,9 @@ const App = () => (
           </Routes>
         </BrowserRouter>
       </AuthProvider>
-      <SpeedInsights/>
+      
     </TooltipProvider>
+    <SpeedInsights />
   </QueryClientProvider>
 );
 
